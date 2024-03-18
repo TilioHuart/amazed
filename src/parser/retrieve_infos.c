cac18f6df9975615683d6e957c989f9322c413ec
Valiance <lucas.ludovic@epitech.eu>
2024-03-18 10:52:18 +0100

    [UPDATE] Parse the user input

--- src/parser/retrieve_infos.c
+++ src/parser/retrieve_infos.c
@@ -5,10 +5,38 @@
 ** Retrieve the user inputs to build the maze
 */
 
+#include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
+#include "my_macros.h"
+#include "my_types.h"
 #include "my.h"
+
+static
+int check_line_info(char *buff)
+{
+    if (buff == NULL)
+        return FAILURE;
+    if (buff[my_strlen(buff) - 1] == '\n')
+        buff[my_strlen(buff) - 1] = '\0';
+    return SUCCESS;
+}
 
 char **retrieve_info(char **map)
 {
+    bool_t has_end = FALSE;
+    char *buff = NULL;
+    size_t size = 0;
+
+    while (!has_end) {
+        if (getline(&buff, &size, stdin) < 0)
+            return NULL;
+        if (check_line_info(buff) == FAILURE)
+            return NULL;
+        if (buff != NULL) {
+            free(buff);
+            buff = NULL;
+        }
+    }
     return map;
 }
