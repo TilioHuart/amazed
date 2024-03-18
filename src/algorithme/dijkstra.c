f498773889d7d1b076588628eb5441a0d2837f17
Tilio <tilio.huart@epitech.eu>
2024-03-18 10:59:34 +0100

    [UPDATE] add funtction to loop in matrix

--- src/algorithme/dijkstra.c
+++ src/algorithme/dijkstra.c
@@ -7,7 +7,14 @@
 
 #include "my_macros.h"
 #include "my.h"
+#include "my_types.h"
 #include <stdio.h>
+
+static
+int find_path_loop(char **matrix)
+{
+    return SUCCESS;
+}
 
 int find_path(char **matrix, int nb_robots)
 {
