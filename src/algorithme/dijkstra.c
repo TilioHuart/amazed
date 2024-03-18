6cc757cfc0e59d132bd393eb5af7b64cabb970c1
Valiance <lucas.ludovic@epitech.eu>
2024-03-18 09:56:07 +0100

    [ADDING FILE] Adding définition of the main algo function

---
+++ src/algorithme/dijkstra.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Amazed
+** File description:
+** Pathfinding algorithme
+*/
+
+#include "my_macros.h"
+#include "my.h"
+
+int find_path(void)
+{
+    return SUCCESS;
+}
---
+++ src/parser/retrieve_infos.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Amazed
+** File description:
+** Retrieve the user inputs to build the maze
+*/
+
+#include <stddef.h>
+#include "my.h"
+
+char **retrieve_info(char **map)
+{
+    return map;
+}
