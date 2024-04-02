5e44e14a513cfd69949e5bd3633e1117ae03454c
Valiance <lucas.ludovic@epitech.eu>
2024-04-02 17:24:55 +0200

    [UPDATE] Update the room when a shorter path is found

--- include/amazed.h
+++ include/amazed.h
@@ -7,14 +7,15 @@
 
 #ifndef AMAZED_H_
     #define AMAZED_H_
+    #include <stddef.h>
 
 typedef struct path_s {
     char *start_room;
     char *end_room;
     char **shortest_path;
     char **current_path;
-    int shortest_path_length;
-    int current_room_number;
+    size_t shortest_path_length;
+    size_t current_room_number;
 } path_t; 
 
 int amazed(void);
