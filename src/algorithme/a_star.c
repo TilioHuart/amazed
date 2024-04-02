fff4d65484f3bcaa30f5db7b5ceb72e988b0c06e
Valiance <lucas.ludovic@epitech.eu>
2024-04-02 14:25:45 +0200

    [UPDATE] Implementing the recursive loop of the algo

--- src/algorithme/a_star.c
+++ src/algorithme/a_star.c
@@ -5,20 +5,27 @@
 ** Pathfinding algorithme
 */
 
+#include <stdio.h>
+#include "map.h"
 #include "my_macros.h"
 #include "my.h"
 #include "my_types.h"
-#include <stdio.h>
 
 static
-int find_path_loop(char **matrix)
+int execute_fbs_algorithme(map_t *map, char *end_room)
 {
-    return SUCCESS;
+    if (map == NULL)
+        return FAILURE;
+    for (size_t i = 0; map->link[i] != NULL; i += 1) {
+        if (my_strcmp(map->link, end_room) != 0)
+            execute_fbs_algorithme(map->link[i], end_room);
+    }
 }
 
-int find_path(char **matrix, UNUSED int nb_robots)
+int find_path(map_t *map, char *end_room, UNUSED int nb_robots)
 {
-    if (matrix == NULL)
-        return display_error("Matrix NULL in algo\n"), FAILURE;
+    if (map == NULL)
+        return display_error("Map null in algo\n"), FAILURE;
+    execute_fbs_algorithme(map, end_room);
     return SUCCESS;
 }
