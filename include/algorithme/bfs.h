4ac5a4c4848a90fefe84393d6d7f21cafdbec1d7
Valiance <lucas.ludovic@epitech.eu>
2024-04-05 00:06:55 +0200

    [UPDATE] Initialise the queue and the visited rooms

---
+++ include/algorithme/bfs.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** Amazed
+** File description:
+** Bfs header
+*/
+
+#ifndef BFS_H_
+    #define BFS_H_
+    #include "map.h"
+
+typedef struct encountered_room_s {
+    map_t *map;
+    struct encountered_room_s *next;
+} encountered_room_t;
+
+typedef struct room_queue_s {
+    map_t *map;
+    struct room_queue *next;
+} room_queue_t;
+
+#endif
--- src/algorithme/bfs_algo.c
+++ src/algorithme/bfs_algo.c
@@ -8,22 +8,13 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "algorithme/bfs.h"
 #include "my_alloc.h"
 #include "amazed.h"
 #include "map.h"
 #include "my_macros.h"
 #include "my.h"
 #include "my_types.h"
-
-typedef struct encountered_room_s {
-    map_t *map;
-    struct encountered_room_s *next;
-} encountered_room_t;
-
-typedef struct room_queue_s {
-    map_t *map;
-    struct room_queue *next;
-} room_queue_t;
 
 static
 int check_visited(map_t const *map, encountered_room_t *visited)
@@ -69,21 +60,29 @@ int add_visited_room(encountered_room_t *visited, map_t *current_room)
 int execute_bfs(map_t *map, char const *end_room)
 {
     encountered_room_t *visited = NULL;
+    room_queue_t *queue = NULL;
 
-    visited = malloc(sizeof(encountered_room_t));
-    if (visited == NULL)
-        return display_error("Unable to allocate memory\n");
-    visited->map = NULL;
-    visited->next = NULL;
     if (end_room == NULL || map == NULL)
         return display_error("Struct null for algorithme\n");
+    visited = malloc(sizeof(encountered_room_t));
+    if (visited == NULL)
+        return display_error("Unable to allocate visited memory\n");
+    visited->map = map;
+    visited->next = NULL;
+    queue = malloc(sizeof(room_queue_t));
+    if (queue == NULL) {
+        free(visited);
+        return display_error("Unable to allocate queue memory\n");
+    }
     if (map->name == NULL || map->link == NULL)
         return display_error("Unable to access the room info\n");
-    for (size_t i = 0; map->link[i] != NULL; i += 1) {
-        if (check_visited(map->link[i], visited) == TRUE)
-            continue;
-        if (add_visited_room(visited, map->link[i]) == FAILURE)
-            continue;
+    while (visited != NULL) {
+        for (size_t i = 0; map->link[i] != NULL; i += 1) {
+            if (check_visited(map->link[i], visited) == TRUE)
+                continue;
+            if (add_visited_room(visited, map->link[i]) == FAILURE)
+                continue;
+        }
     }
     return SUCCESS;
 }
